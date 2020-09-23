c763ac497360a5e103046425327876fc0aae483e
William Lin <wlin26@yahoo.com>
2020-09-22 20:33:04 -0400

    Added excel file with chart and pyplot scatter

---
+++ src/plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+date = []
+temp = []
+
+with open('./data/data.csv') as data_file:
+    csv_reader = csv.reader(data_file, delimiter=',')
+    for row in csv_reader:
+        date.append(row[0])
+        temp.append(row[1])
+
+print('Done Parsing Data')
+
+plt.xticks(np.arange(0, 100, 1))
+plt.scatter(date[:100], temp[:100], c='blue')
+plt.show()
